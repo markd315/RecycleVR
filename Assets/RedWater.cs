a086a7002ca94117496c5d1e696b12218f05a1c6
markd315 <markd315@gmail.com>
2018-09-24 21:24:36 -0400

    Fixed Scene switching, lighting remains a concern

---
+++ Assets/RedWater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedWater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().material.color = new Color(1f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
