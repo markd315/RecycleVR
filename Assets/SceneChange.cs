6488f80babdcfc7fc1a746eea30890d83ecfe2a4
markd315 <markd315@gmail.com>
2018-09-24 20:40:14 -0400

    Scene switcher on player object

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+ using UnityEngine;
+ using UnityEngine.SceneManagement;
+ 
+ public class SceneChange : MonoBehaviour
+ {
+     public float delay = 15;
+     public string NewLevel= "TrashScene";
+     void Start()
+     {
+         StartCoroutine(LoadLevelAfterDelay(delay));
+     }
+ 
+     IEnumerator LoadLevelAfterDelay(float delay)
+     {
+         yield return new WaitForSeconds(delay);
+         SceneManager.LoadScene(NewLevel);
+     }
+ }
